b45037dde6c9a0637a4e67066ddcab0140c98646
Oumy Mbaye <okmbaye@Oumys-MacBook-Air.local>
2024-12-03 15:51:08 -0600

    finished main screen

--- gui.py
+++ gui.py
@@ -33,31 +33,161 @@ class LaundryGui:
         self.window = window
         self.border = border  
 
+        # Curr_System init
+        self.current_system = LSystem()
+
         # Initialize Pygame and Text Capabilities 
         pygame.init()
         pygame.font.get_init()
 
         # Set window title
-        pygame.display.set_caption("Laundry Race")
+        pygame.display.set_caption("Tide-Y Time")
 
         # Initialize display
         self.surface = pygame.display.set_mode((window + border,
-                                                window + border))
+                                                window - border))
         self.light_load = 0
         self.dark_load = 0   
         self.clock = pygame.time.Clock()
         self.event_loop()
-        
-
 
     def draw_main_screen(self):
         """
         Draw the main program screen.
         """
         #Background
-        self.surface.fill((120, 120, 120))
-    
-        
+        self.surface.fill((237, 185, 204))
+
+        # Draw the rectangle representing the compartment, folding board, and laundry baskets
+        tide_rect = (45, 450, 135, 125)
+        light_basket_rect = (198, 320, 150, 255)
+        dark_basket_rect = (366, 320, 150, 255)
+        folding_rect = (534, 450, 195, 125)
+
+        pygame.draw.rect(self.surface, (219, 151, 176), tide_rect, 0, 10)
+        pygame.draw.rect(self.surface, (219, 151, 176), light_basket_rect, 0, 10)
+        pygame.draw.rect(self.surface, (219, 151, 176), dark_basket_rect, 0, 10)
+        pygame.draw.rect(self.surface, (219, 151, 176), folding_rect, 0, 10)
+
+        # Each basket has 5 levels of shading to visualize fullness
+        lbask_level1 = (209, 332, 125, 61)
+        lbask_level2 = (209, 379, 125, 61)
+        lbask_level3 = (209, 420, 125, 61)
+        lbask_level4 = (209, 471, 125, 61)
+        lbask_level5 = (209, 522, 125, 41)
+        lbask_levels = [lbask_level1, lbask_level2, lbask_level3, lbask_level4, lbask_level5][::-1]
+
+        drbask_level1 = (377, 332, 125, 61)
+        drbask_level2 = (377, 379, 125, 61)
+        drbask_level3 = (377, 420, 125, 61)
+        drbask_level4 = (377, 471, 125, 61)
+        drbask_level5 = (377, 522, 125, 41)
+        drbask_levels = [drbask_level1, drbask_level2, drbask_level3, drbask_level4, drbask_level5][::-1]
+
+        # Get the level of laundry for each basket 
+        lbask_height =  self.current_system.light_basket.get_count() // 5
+        drbask_height = self.current_system.light_basket.get_count() // 5
+
+        for level in range(5):
+            # Draw how full the basket is
+            if level < lbask_height:
+                pygame.draw.rect(self.surface, (186, 110, 140), lbask_levels[level], 0, 5) 
+            else:
+                pygame.draw.rect(self.surface, (199, 135, 160), lbask_levels[level], 0, 5)
+
+            # Draw how full the basket is
+            if level < drbask_height:
+                pygame.draw.rect(self.surface, (186, 110, 140), drbask_levels[level], 0, 5) 
+            else:
+                pygame.draw.rect(self.surface, (199, 135, 160), drbask_levels[level], 0, 5)
+
+        # Draw bar for when you have to do your laundry based on combined compacity
+        status_bar = (45, 200, 675, 45)
+        inner_bar = (50, 205, 665, 35)
+        capacity_marker_1 = (216, 205, 5, 35)
+        capacity_marker_2 = (382, 205, 5, 35)
+        capacity_marker_3 = (548, 205, 5, 35)
+
+        pygame.draw.rect(self.surface, (219, 151, 176), status_bar, 0, 12)
+        pygame.draw.rect(self.surface, (199, 135, 160), inner_bar, 0, 8)
+
+        # Fill in levels based on total capacity
+        capacity_level = (self.current_system.light_basket.get_count() + self.current_system.dark_basket.get_count() + 2) // 13
+        cap_level_1 = (50, 205, 166, 35)
+        cap_level_2 = (221, 205, 166, 35)
+        cap_level_3 = (387, 205, 166, 35)
+        cap_level_4 = (553, 205, 160, 35)
+        cap_levels =[cap_level_1, cap_level_2, cap_level_3, cap_level_4]
+
+        for level in range(4):
+            if level < capacity_level:
+                pygame.draw.rect(self.surface, (144, 153, 89), cap_levels[level], 0, 8)
+            else:
+                pygame.draw.rect(self.surface, (199, 135, 160), cap_levels[level], 0, 8)
+
+        # Draw markers over top levels
+        pygame.draw.rect(self.surface, (219, 151, 176), capacity_marker_1, 0, 0)
+        pygame.draw.rect(self.surface, (219, 151, 176), capacity_marker_2, 0, 0)
+        pygame.draw.rect(self.surface, (219, 151, 176), capacity_marker_3, 0, 0)
+
+        # Draw accents for tide pod compartment and folding board
+        accent1_tide_rect = (59, 475, 108, 77)
+        pygame.draw.rect(self.surface, (199, 135, 160), accent1_tide_rect, 0, 4)
+
+        accent1_activity_stat = (554, 475, 155, 75)
+        if(self.current_system.timer.is_running):
+            pygame.draw.rect(self.surface, (178, 186, 132), accent1_activity_stat, 0, 4)
+        else:
+            pygame.draw.rect(self.surface, (199, 135, 160), accent1_activity_stat, 0, 4)
+
+        ###### Handling Text Objects ########
+
+        # Title Text
+        title_text = pygame.font.SysFont("Impact", 85)
+        title_render = title_text.render("Tide-Y Time", True, (163, 100, 124))
+        title_object = title_render.get_rect()
+        title_object.center = 380 , 100
+        self.surface.blit(title_render, title_object)
+
+        # Basket Capacity Text
+        lbasket_text = pygame.font.SysFont("Sans Serif", 20)
+        lbasket_render = lbasket_text.render(f"LIGHT TOTAL: {self.current_system.light_basket.get_count()}/25", True, (163, 100, 124))
+        lbasket_object = lbasket_render.get_rect()
+        lbasket_object.center = 273, 595
+        self.surface.blit(lbasket_render, lbasket_object)
+
+        drbasket_text = pygame.font.SysFont("Sans Serif", 20)
+        drbasket_render = drbasket_text.render(f"DARK TOTAL: {self.current_system.dark_basket.get_count()}/25", True, (163, 100, 124))
+        drbasket_object = drbasket_render.get_rect()
+        drbasket_object.center = 441, 595
+        self.surface.blit(drbasket_render, drbasket_object)
+
+        # Activity Text 
+        active_text = pygame.font.SysFont("Impact", 25)
+
+        if(self.current_system.timer.is_running):
+            active_render = active_text.render("TIME RUNNING", True, (163, 100, 124))
+        else:
+            active_render = active_text.render("TIME OFF", True, (163, 100, 124))
+        active_object = active_render.get_rect()
+        active_object.center = 631, 512
+        self.surface.blit(active_render, active_object)
+
+        # Press Prompt Text
+        press_text = pygame.font.SysFont("Sans Serif", 20)
+        press_render = press_text.render("GET TIDE POD REC!", True, (163, 100, 124))
+        press_object = press_render.get_rect()
+        press_object.center = 112, 432
+        self.surface.blit(press_render, press_object)
+
+        # Capacity Text
+        capacity_text = pygame.font.SysFont("Sans Serif", 20)
+        capacity_render = press_text.render(f"TIDE-Y CHECK - %{(self.current_system.light_basket.get_count() + self.current_system.dark_basket.get_count()) // 50} Till You're at Capacity", True, (163, 100, 124))
+        capacity_object = press_render.get_rect()
+        capacity_object.center = 115, 185
+        self.surface.blit(capacity_render, capacity_object)
+
+
     def draw_tide_pod_rect(self, curr_system: LSystem) -> None:
         """
         When the user selects a load draw the recommended amount of tide pods to use.
